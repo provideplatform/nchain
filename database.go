35e143422c6e08a209852573b28d065471f20824
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-04-16 23:33:32 -0400

    Add index on transactions.status

--- database.go
+++ database.go
@@ -32,6 +32,7 @@ func migrateSchema() {
 
 		db.AutoMigrate(&Transaction{})
 		db.Model(&Transaction{}).AddIndex("idx_transactions_application_id", "application_id")
+		db.Model(&Transaction{}).AddIndex("idx_transactions_status", "status")
 		db.Model(&Transaction{}).AddIndex("idx_transactions_user_id", "user_id")
 		db.Model(&Transaction{}).AddUniqueIndex("idx_transactions_hash", "hash")
 		db.Model(&Transaction{}).AddForeignKey("network_id", "networks(id)", "SET NULL", "CASCADE")
