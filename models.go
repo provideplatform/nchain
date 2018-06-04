3a4d861425b099c085fc9c75ff4001194af05eb6
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-06-04 02:09:56 -0400

    Add protocol-specific chain_id to networks

--- models.go
+++ models.go
@@ -33,6 +33,7 @@ type Network struct {
 	IsProduction  *bool            `sql:"not null" json:"is_production"`
 	Cloneable     *bool            `sql:"not null" json:"cloneable"`
 	Enabled       *bool            `sql:"not null" json:"enabled"`
+	ChainID       *string          `json:"chain_id"`                     // protocol-specific chain id
 	SidechainID   *uuid.UUID       `sql:"type:uuid" json:"sidechain_id"` // network id used as the transactional sidechain (or null)
 	NetworkID     *uuid.UUID       `sql:"type:uuid" json:"network_id"`   // network id used as the parent
 	Config        *json.RawMessage `sql:"type:json" json:"config"`
