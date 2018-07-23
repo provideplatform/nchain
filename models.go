eb2a83273c10411c86a311372fca7f80dfa1f359
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-07-23 05:06:59 -0400

    Remove network-related items from cloneable network node whitelist

--- models.go
+++ models.go
@@ -31,7 +31,7 @@ import (
 	"github.com/kthomas/go.uuid"
 )
 
-var defaultNetworkNodeCloneConfigWhitelist = []string{"cloneable_cfg", "credentials", "default_json_rpc_port", "default_websocket_port", "rc.d", "env", "is_ethereum_network", "protocol_id", "engine_id", "target_id", "provider_id", "role", "region", "chainspec_url"}
+var defaultNetworkNodeCloneConfigWhitelist = []string{"credentials", "default_json_rpc_port", "default_websocket_port", "rc.d", "env", "protocol_id", "engine_id", "target_id", "provider_id", "role", "region"}
 var defaultNetworkNodeConfigMarshalingBlacklist = []string{"credentials"}
 
 const hostReachabilityTimeout = time.Minute * 5
