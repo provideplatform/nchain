262db34c74c4d466ed9260db7d88711df38d20f9
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-10-08 11:07:45 -0400

    Manually revert IsUnique() check around tx signing

--- models.go
+++ models.go
@@ -1792,11 +1792,6 @@ func (c *Contract) executeEthereumContract(network *Network, tx *Transaction, me
 			if publicKeyOk && privateKeyOk {
 				Log.Debugf("Attempting to execute %s on contract: %s; arbitrarily-provided signer for tx: %s; gas supplied: %v", methodDescriptor, c.ID, publicKey, gas)
 				tx.SignedTx, tx.Hash, err = provide.SignTx(network.ID.String(), network.rpcURL(), publicKey.(string), privateKey.(string), tx.To, tx.Data, tx.Value.BigInt(), uint64(gas))
-				isUnique, _ := tx.IsUnique()
-				if !isUnique {
-					tx.updateStatus(DatabaseConnection(), "duplicate tx hash", &desc)
-					return nil, nil, fmt.Errorf("Duplicate tx hash: %s", *tx.Hash)
-				}
 				if err == nil {
 					if signedTx, ok := tx.SignedTx.(*types.Transaction); ok {
 						err = provide.BroadcastSignedTx(network.ID.String(), network.rpcURL(), signedTx)
