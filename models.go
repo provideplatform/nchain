edb2ef4087b2ea0829a40b3efdb8f9cfbf4e30a2
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-10-08 09:37:43 -0400

    Revert "Prevent fetching tx receipt if transaction is non-unique"
    This reverts commit f9b57de1e52e975a7a9baea72ec2968139b296ce.

--- models.go
+++ models.go
@@ -2721,12 +2721,9 @@ func (t *Transaction) Create() bool {
 					desc := err.Error()
 					t.updateStatus(db, "failed", &desc)
 				} else {
-					isUnique, _ := t.IsUnique()
-					if isUnique {
-						txReceiptMsg, _ := json.Marshal(t)
-						natsConnection := getNatsStreamingConnection()
-						natsConnection.Publish(natsTxReceiptSubject, txReceiptMsg)
-					}
+					txReceiptMsg, _ := json.Marshal(t)
+					natsConnection := getNatsStreamingConnection()
+					natsConnection.Publish(natsTxReceiptSubject, txReceiptMsg)
 				}
 			}
 			return rowsAffected > 0 && len(t.Errors) == 0
