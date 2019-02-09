eeeb2dfcfa217d319bf9daeaef25e28c8ec412ec
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2019-02-09 02:49:13 -0500

    Rename dev scripts

--- scripts/kill-dev.sh
+++ scripts/kill-dev.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 pkill nats-streaming-server
 pkill gnatsd 
 pg_ctl -D /usr/local/var/postgres stop -s -m fast
--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 nats-streaming-server -cid provide -auth testtoken -p 4222 &
 gnatsd -auth testtoken -p 4221 &
 pg_ctl -D /usr/local/var/postgres start &
