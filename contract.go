provideplatform/nchain

commit 2a27af89c5f2607231c9227a36d1de57df5f0c93

Cleanup arbitrary nonce passing throughout tx lifecycle

contract.go
@@ -410,7 +410,7 @@ func (c *Contract) Execute(ref *string, wallet *Wallet, value *big.Int, method s
 	txParams["gas"] = gas
 
 	if nonce != nil {
-		txParams["nonce"] = nonce
+		txParams["nonce"] = *nonce
 	}
 
 	txParamsJSON, _ := json.Marshal(txParams)
@@ -436,17 +436,17 @@ func (c *Contract) Execute(ref *string, wallet *Wallet, value *big.Int, method s
 		err = fmt.Errorf("unsupported network: %s", *network.Name)
 	}
 
-	accessedAt := time.Now()
-	go func() {
-		c.AccessedAt = &accessedAt
-		db.Save(c)
-	}()
-
 	if err != nil {
 		desc := err.Error()
 		tx.updateStatus(db, "failed", &desc)
 		return nil, fmt.Errorf("Unable to execute %s contract; %s", *network.Name, err.Error())
 	}
+
+	accessedAt := time.Now()
+	go func() {
+		c.AccessedAt = &accessedAt
+		db.Save(c)
+	}()
 
 	if tx.Response == nil {
 		tx.Response = &ContractExecutionResponse{

contract_execution.go
@@ -36,22 +36,25 @@ type ContractExecutionResponse struct {
 
 // Execute an ephemeral ContractExecution
 func (e *ContractExecution) Execute() (interface{}, error) {
-	var _abi *abi.ABI
-	if __abi, abiOk := e.ABI.(abi.ABI); abiOk {
-		_abi = &__abi
-	} else if e.Contract != nil {
-		__abi, err := e.Contract.readEthereumContractAbi()
-		if err != nil {
-			Log.Warningf("Cannot attempt contract execution without ABI")
-			return nil, err
+	if e.ABI != nil {
+		// isEthereumContract = true
+		var _abi *abi.ABI
+		if __abi, abiOk := e.ABI.(abi.ABI); abiOk {
+			_abi = &__abi
+		} else if e.Contract != nil {
+			__abi, err := e.Contract.readEthereumContractAbi()
+			if err != nil {
+				Log.Warningf("Cannot attempt contract execution without ABI")
+				return nil, err
+			}
+			_abi = __abi
 		}
-		_abi = __abi
-	}
 
-	if _abi != nil {
-		if mthd, ok := _abi.Methods[e.Method]; ok {
-			if mthd.Const {
-				return e.Contract.Execute(e.Ref, e.Wallet, e.Value, e.Method, e.Params, 0, nil)
+		if _abi != nil {
+			if mthd, ok := _abi.Methods[e.Method]; ok {
+				if mthd.Const {
+					return e.Contract.Execute(e.Ref, e.Wallet, e.Value, e.Method, e.Params, 0, nil)
+				}
 			}
 		}
 	}

contract_handlers.go
@@ -278,6 +278,14 @@ func contractExecutionHandler(c *gin.Context) {
 		return
 	}
 
+	params := map[string]interface{}{}
+	err = json.Unmarshal(buf, &params)
+	if err != nil {
+		err = fmt.Errorf("Failed to parse JSON-RPC params; %s", err.Error())
+		renderError(err.Error(), 400, c)
+		return
+	}
+
 	db := dbconf.DatabaseConnection()
 
 	contractID := c.Param("id")
@@ -287,13 +295,6 @@ func contractExecutionHandler(c *gin.Context) {
 		rpcNetworkID, err := uuid.FromString(rpcNetworkIDStr)
 		if err != nil {
 			err = fmt.Errorf("Failed to parse RPC network id as valid uuid: %s; %s", rpcNetworkIDStr, err.Error())
-			renderError(err.Error(), 400, c)
-			return
-		}
-		params := map[string]interface{}{}
-		err = json.Unmarshal(buf, &params)
-		if err != nil {
-			err = fmt.Errorf("Failed to parse JSON-RPC params; %s", err.Error())
 			renderError(err.Error(), 400, c)
 			return
 		}
@@ -359,6 +360,18 @@ func contractExecutionHandler(c *gin.Context) {
 		wallet := &Wallet{}
 		wallet.Address = *execution.WalletAddress
 		execution.Wallet = wallet
+	}
+
+	gas, gasOk := params["gas"].(float64)
+	nonce, nonceOk := params["nonce"].(float64)
+
+	if gasOk {
+		execution.Gas = &gas
+	}
+
+	if nonceOk {
+		nonceUint := uint64(nonce)
+		execution.Nonce = &nonceUint
 	}
 
 	executionResponse, err := execution.Execute()

tx.go
@@ -341,10 +341,16 @@ func (t *Transaction) sign(db *gorm.DB, network *Network, wallet *Wallet) error 
 			gas = float64(0)
 		}
 
+		var nonce *uint64
+		if nonceFloat, nonceOk := params["nonce"].(float64); nonceOk {
+			nonceUint := uint64(nonceFloat)
+			nonce = &nonceUint
+		}
+
 		if wallet.PrivateKey != nil {
 			privateKey, _ := DecryptECDSAPrivateKey(*wallet.PrivateKey, GpgPrivateKey, WalletEncryptionKey)
 			_privateKey := hex.EncodeToString(ethcrypto.FromECDSA(privateKey))
-			t.SignedTx, t.Hash, err = provide.EVMSignTx(network.ID.String(), network.rpcURL(), wallet.Address, _privateKey, t.To, t.Data, t.Value.BigInt(), nil, uint64(gas))
+			t.SignedTx, t.Hash, err = provide.EVMSignTx(network.ID.String(), network.rpcURL(), wallet.Address, _privateKey, t.To, t.Data, t.Value.BigInt(), nonce, uint64(gas))
 		} else {
 			err = fmt.Errorf("Unable to sign tx; no private key for wallet: %s", wallet.ID)
 		}

commit 99286e4edb5108a3f055c13930cfeca15d518aa7

Fix unreachable constant contract execution regression caused in 2a27af89c5f2607231c9227a36d1de57df5f0c93

contract.go
@@ -139,6 +139,17 @@ func (c *Contract) Compile() (*provide.CompiledArtifact, error) {
 	return artifact, nil
 }
 
+// GetNetwork - retrieve the associated contract network
+func (c *Contract) GetNetwork() (*Network, error) {
+	db := dbconf.DatabaseConnection()
+	var network = &Network{}
+	db.Model(c).Related(&network)
+	if network == nil {
+		return nil, fmt.Errorf("Failed to retrieve contract network for contract: %s", c.ID)
+	}
+	return network, nil
+}
+
 // ParseParams - parse the original JSON params used for contract creation
 func (c *Contract) ParseParams() map[string]interface{} {
 	params := map[string]interface{}{}

contract_execution.go
@@ -36,25 +36,25 @@ type ContractExecutionResponse struct {
 
 // Execute an ephemeral ContractExecution
 func (e *ContractExecution) Execute() (interface{}, error) {
-	if e.ABI != nil {
-		// isEthereumContract = true
-		var _abi *abi.ABI
-		if __abi, abiOk := e.ABI.(abi.ABI); abiOk {
-			_abi = &__abi
-		} else if e.Contract != nil {
-			__abi, err := e.Contract.readEthereumContractAbi()
-			if err != nil {
-				Log.Warningf("Cannot attempt contract execution without ABI")
+	var _abi *abi.ABI
+	if execABI, abiOk := e.ABI.(abi.ABI); abiOk {
+		_abi = &execABI
+	} else if e.Contract != nil {
+		execABI, err := e.Contract.readEthereumContractAbi()
+		if err != nil {
+			network, err := e.Contract.GetNetwork()
+			if err != nil || network.isEthereumNetwork() {
+				Log.Warningf("Cannot attempt EVM-based contract execution without ABI")
 				return nil, err
 			}
-			_abi = __abi
 		}
+		_abi = execABI
+	}
 
-		if _abi != nil {
-			if mthd, ok := _abi.Methods[e.Method]; ok {
-				if mthd.Const {
-					return e.Contract.Execute(e.Ref, e.Wallet, e.Value, e.Method, e.Params, 0, nil)
-				}
+	if _abi != nil {
+		if mthd, ok := _abi.Methods[e.Method]; ok {
+			if mthd.Const {
+				return e.Contract.Execute(e.Ref, e.Wallet, e.Value, e.Method, e.Params, 0, nil)
 			}
 		}
 	}