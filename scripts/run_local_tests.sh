70e06ad56377a05c30a5925803089256cd6b8b3f
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2019-03-24 06:56:13 -0400

    Cleanup run_local_test.sh by using -ginkgo flags

--- scripts/run_local_tests.sh
+++ scripts/run_local_tests.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-go get github.com/onsi/ginkgo/ginkgo
-go get github.com/onsi/gomega/...
-
 DB_NAME=goldmine_test
-
 PGPASSWORD=goldmine dropdb -U goldmine goldmine_test
 PGPASSWORD=goldmine createdb -O goldmine -U goldmine goldmine_test 
 PGPASSWORD=goldmine psql -Ugoldmine goldmine_test < db/networks_test.sql
 
-rm goldmine
+rm goldmine > /dev/null
 go build .
-#NATS_TOKEN=testtoken NATS_URL=nats://localhost:4221 NATS_STREAMING_URL=nats://localhost:4222 NATS_CLUSTER_ID=provide NATS_STREAMING_CONCURRENCY=1 GIN_MODE=release DATABASE_NAME=${DB_NAME} DATABASE_USER=goldmine DATABASE_PASSWORD=goldmine DATABASE_HOST=localhost AMQP_URL=amqp://ticker:ticker@10.0.0.126 AMQP_EXCHANGE=ticker LOG_LEVEL=DEBUG /usr/local/bin/go test -timeout 30s
-NATS_TOKEN=testtoken NATS_URL=nats://localhost:4221 NATS_STREAMING_URL=nats://localhost:4222 NATS_CLUSTER_ID=provide NATS_STREAMING_CONCURRENCY=1 GIN_MODE=release DATABASE_NAME=${DB_NAME} DATABASE_USER=goldmine DATABASE_PASSWORD=goldmine DATABASE_HOST=localhost AMQP_URL=amqp://ticker:ticker@10.0.0.126 AMQP_EXCHANGE=ticker LOG_LEVEL=DEBUG ginkgo -r -v --randomizeAllSpecs --randomizeSuites --cover --trace --progress --race --compilers=2
+NATS_TOKEN=testtoken NATS_URL=nats://localhost:4221 NATS_STREAMING_URL=nats://localhost:4222 NATS_CLUSTER_ID=provide NATS_STREAMING_CONCURRENCY=1 GIN_MODE=release DATABASE_NAME=${DB_NAME} DATABASE_USER=goldmine DATABASE_PASSWORD=goldmine DATABASE_HOST=localhost AMQP_URL=amqp://ticker:ticker@10.0.0.126 AMQP_EXCHANGE=ticker LOG_LEVEL=DEBUG /usr/local/bin/go test -v -race -cover -timeout 30s -ginkgo.randomizeAllSpecs -ginkgo.progress -ginkgo.trace
