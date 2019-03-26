69749e6f503510e552c4900909baae1e9f4c2027
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2019-03-26 01:30:02 -0400

    Drop -P from createuser call

--- scripts/ci-process.sh
+++ scripts/ci-process.sh
@@ -194,7 +194,7 @@ unit_test()
 {
     echo '....[PRVD] Setting up Prerequisites for Test Harness....'
     DB_NAME=goldmine_test
-    createuser goldmine -P goldmine
+    createuser goldmine
     createdb goldmine_test -O goldmine
     PGPASSWORD=goldmine dropdb -U goldmine goldmine_test >/dev/null
     PGPASSWORD=goldmine createdb -O goldmine -U goldmine goldmine_test >/dev/null
