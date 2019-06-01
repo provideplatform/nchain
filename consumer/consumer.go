e1bcb91d8db5b4e023f8d8505f4bd1b8af56dabf
Kyle Thomas <kyle@provide.services>
2019-06-01 03:33:29 -0400

    Setup conditional deadlettering based on basic per-message timeout

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -2,6 +2,7 @@ package consumer
 
 import (
 	"sync"
+	"time"
 
 	natsutil "github.com/kthomas/go-natsutil"
 	stan "github.com/nats-io/go-nats-streaming"
@@ -46,6 +47,15 @@ func subscribeNatsStreaming() {
 	}
 }
 
+// AttemptNack tries to Nack the given message if it meets basic time-based deadlettering
+func AttemptNack(msg *stan.Msg, timeout int64) {
+	if ShouldDeadletter(msg, timeout) {
+		common.Log.Debugf("Nacking redelivered %d-byte message after %dms timeout: %s", msg.Size(), timeout, msg.Subject)
+		Nack(msg)
+	}
+}
+
+// Nack the given message
 func Nack(msg *stan.Msg) {
 	if msg.Redelivered {
 		common.Log.Warningf("Nacking redelivered %d-byte message without checking subject-specific deadletter business logic on subject: %s", msg.Size(), msg.Subject)
@@ -54,4 +64,9 @@ func Nack(msg *stan.Msg) {
 	} else {
 		common.Log.Debugf("nack() attempted but given NATS message has not yet been redelivered on subject: %s", msg.Subject)
 	}
+}
+
+// ShouldDeadletter determines if a given message should be deadlettered
+func ShouldDeadletter(msg *stan.Msg, deadletterTimeout int64) bool {
+	return msg.Redelivered && time.Now().Unix()-msg.Timestamp >= deadletterTimeout
 }
