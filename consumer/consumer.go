415a900f8f97830effdae6517d6b8705abc4f4bd
Kyle Thomas <kyle@provide.services>
2019-08-21 13:03:28 -0400

    Revert "Add NATS Nack helper back to consumer"
    This reverts commit 887c8a3f7833601c1b0ea87dd3d76be26e7c9628.

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -2,10 +2,6 @@ package consumer
 
 import (
 	"sync"
-
-	natsutil "github.com/kthomas/go-natsutil"
-	stan "github.com/nats-io/stan.go"
-	"github.com/provideapp/goldmine/common"
 )
 
 const natsDefaultClusterID = "provide"
@@ -21,14 +17,3 @@ var (
 		// "PRVD-USD", // FIXME-- pull from tokens database
 	}
 )
-
-// Nack the given message
-func Nack(msg *stan.Msg) {
-	if msg.Redelivered {
-		common.Log.Warningf("Nacking redelivered %d-byte message without checking subject-specific deadletter business logic on subject: %s", msg.Size(), msg.Subject)
-		conn, _ := common.GetSharedNatsStreamingConnection()
-		natsutil.Nack(conn, msg)
-	} else {
-		common.Log.Debugf("Nack() attempted but given NATS message has not yet been redelivered on subject: %s", msg.Subject)
-	}
-}
