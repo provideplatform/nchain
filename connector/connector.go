52f7e68a9e378617042c0904e1c2de42b7e8c588
Kyle Thomas <kyle@provide.services>
2019-09-15 23:11:13 -0400

    Add encrypted config to connectors

--- connector/connector.go
+++ connector/connector.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	dbconf "github.com/kthomas/go-db-config"
+	pgputil "github.com/kthomas/go-pgputil"
 	uuid "github.com/kthomas/go.uuid"
 	"github.com/provideapp/goldmine/common"
 	provide "github.com/provideservices/provide-go"
@@ -24,12 +25,13 @@ func init() {
 // in the future it may represent a logical connection to services of other types
 type Connector struct {
 	provide.Model
-	ApplicationID *uuid.UUID       `sql:"type:uuid" json:"application_id"`
-	NetworkID     uuid.UUID        `sql:"not null;type:uuid" json:"network_id"`
-	Name          *string          `sql:"not null" json:"name"`
-	Type          *string          `sql:"not null" json:"type"`
-	Config        *json.RawMessage `sql:"type:json" json:"config"`
-	AccessedAt    *time.Time       `json:"accessed_at"`
+	ApplicationID   *uuid.UUID       `sql:"type:uuid" json:"application_id"`
+	NetworkID       uuid.UUID        `sql:"not null;type:uuid" json:"network_id"`
+	Name            *string          `sql:"not null" json:"name"`
+	Type            *string          `sql:"not null" json:"type"`
+	Config          *json.RawMessage `sql:"type:json" json:"config"`
+	EncryptedConfig *string          `sql:"type:bytea" json:"-"`
+	AccessedAt      *time.Time       `json:"accessed_at"`
 }
 
 // ParseConfig - parse the original JSON params used for Connector creation
@@ -45,6 +47,69 @@ func (c *Connector) ParseConfig() map[string]interface{} {
 	return cfg
 }
 
+func (c *Connector) decryptedConfig() (map[string]interface{}, error) {
+	decryptedParams := map[string]interface{}{}
+	if c.EncryptedConfig != nil {
+		encryptedConfigJSON, err := pgputil.PGPPubDecrypt([]byte(*c.EncryptedConfig))
+		if err != nil {
+			common.Log.Warningf("Failed to decrypt encrypted load balancer config; %s", err.Error())
+			return decryptedParams, err
+		}
+
+		err = json.Unmarshal(encryptedConfigJSON, &decryptedParams)
+		if err != nil {
+			common.Log.Warningf("Failed to unmarshal decrypted load balancer config; %s", err.Error())
+			return decryptedParams, err
+		}
+	}
+	return decryptedParams, nil
+}
+
+func (c *Connector) encryptConfig() bool {
+	if c.EncryptedConfig != nil {
+		encryptedConfig, err := pgputil.PGPPubEncrypt([]byte(*c.EncryptedConfig))
+		if err != nil {
+			common.Log.Warningf("Failed to encrypt load balancer config; %s", err.Error())
+			c.Errors = append(c.Errors, &provide.Error{
+				Message: common.StringOrNil(err.Error()),
+			})
+			return false
+		}
+		c.EncryptedConfig = common.StringOrNil(string(encryptedConfig))
+	}
+	return true
+}
+
+func (c *Connector) setConfig(cfg map[string]interface{}) {
+	cfgJSON, _ := json.Marshal(cfg)
+	_cfgJSON := json.RawMessage(cfgJSON)
+	c.Config = &_cfgJSON
+}
+
+func (c *Connector) setEncryptedConfig(params map[string]interface{}) {
+	paramsJSON, _ := json.Marshal(params)
+	_paramsJSON := string(json.RawMessage(paramsJSON))
+	c.EncryptedConfig = &_paramsJSON
+	c.encryptConfig()
+}
+
+func (c *Connector) sanitizeConfig() {
+	cfg := c.ParseConfig()
+
+	encryptedCfg, err := c.decryptedConfig()
+	if err != nil {
+		encryptedCfg = map[string]interface{}{}
+	}
+
+	if credentials, credentialsOk := cfg["credentials"]; credentialsOk {
+		encryptedCfg["credentials"] = credentials
+		delete(cfg, "credentials")
+	}
+
+	c.setConfig(cfg)
+	c.setEncryptedConfig(encryptedCfg)
+}
+
 // Create and persist a new Connector
 func (c *Connector) Create() bool {
 	db := dbconf.DatabaseConnection()
@@ -52,6 +117,8 @@ func (c *Connector) Create() bool {
 	if !c.Validate() {
 		return false
 	}
+
+	c.sanitizeConfig()
 
 	if db.NewRecord(c) {
 		result := db.Create(&c)
