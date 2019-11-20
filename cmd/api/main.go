46d32a845be8cd72d2f6bc2966a3245abf229448
Kyle Thomas <kyle@provide.services>
2019-11-19 22:27:15 -0500

    Gracefully shutdown gin on signal

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	redisutil "github.com/kthomas/go-redisutil"
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync/atomic"
+	"syscall"
+	"time"
 
-	provide "github.com/provideservices/provide-go"
+	"github.com/gin-gonic/gin"
 
 	"github.com/provideapp/goldmine/common"
 	"github.com/provideapp/goldmine/connector"
@@ -17,24 +22,79 @@ import (
 	"github.com/provideapp/goldmine/token"
 	"github.com/provideapp/goldmine/tx"
 	"github.com/provideapp/goldmine/wallet"
+
+	redisutil "github.com/kthomas/go-redisutil"
+	provide "github.com/provideservices/provide-go"
+)
+
+const runloopSleepInterval = 250 * time.Millisecond
+const runloopTickInterval = 5000 * time.Millisecond
+
+var (
+	cancelF     context.CancelFunc
+	closing     uint32
+	shutdownCtx context.Context
+	sigs        chan os.Signal
+
+	srv *http.Server
 )
 
 func init() {
-	redisutil.RequireRedis()
-}
-
-func main() {
 	if common.ConsumeNATSStreamingSubscriptions {
 		common.Log.Panicf("Dedicated API instance started with CONSUME_NATS_STREAMING_SUBSCRIPTIONS=true")
 		return
 	}
 
-	common.Log.Debugf("Running dedicated API instance main()")
-
 	consumer.RunAPIUsageDaemon()
 	filter.CacheTxFilters()
+	redisutil.RequireRedis()
+}
 
-	r := gin.Default()
+func main() {
+	common.Log.Debugf("starting goldmine API...")
+	installSignalHandlers()
+
+	runAPI()
+
+	timer := time.NewTicker(runloopTickInterval)
+	defer timer.Stop()
+
+	for !shuttingDown() {
+		select {
+		case <-timer.C:
+			// tick... no-op
+		case sig := <-sigs:
+			common.Log.Debugf("received signal: %s", sig)
+			srv.Shutdown(shutdownCtx)
+			shutdown()
+		case <-shutdownCtx.Done():
+			close(sigs)
+		default:
+			time.Sleep(runloopSleepInterval)
+		}
+	}
+
+	common.Log.Debug("exiting goldmine API")
+	cancelF()
+}
+
+func installSignalHandlers() {
+	common.Log.Debug("installing signal handlers for goldmine API")
+	sigs = make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	shutdownCtx, cancelF = context.WithCancel(context.Background())
+}
+
+func shutdown() {
+	if atomic.AddUint32(&closing, 1) == 1 {
+		common.Log.Debug("shutting down goldmine API")
+		cancelF()
+	}
+}
+
+func runAPI() {
+	r := gin.New()
+	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 	r.Use(provide.CORSMiddleware())
 	r.Use(provide.TrackAPICalls())
@@ -50,13 +110,24 @@ func main() {
 
 	r.GET("/status", statusHandler)
 
-	if common.ShouldServeTLS() {
-		r.RunTLS(common.ListenAddr, common.CertificatePath, common.PrivateKeyPath)
-	} else {
-		r.Run(common.ListenAddr)
+	srv = &http.Server{
+		Addr:    common.ListenAddr,
+		Handler: r,
 	}
+
+	if common.ShouldServeTLS() {
+		go srv.ListenAndServeTLS(common.CertificatePath, common.PrivateKeyPath)
+	} else {
+		go srv.ListenAndServe()
+	}
+
+	common.Log.Debugf("Listening on %s", common.ListenAddr)
 }
 
 func statusHandler(c *gin.Context) {
 	provide.Render(nil, 204, c)
+}
+
+func shuttingDown() bool {
+	return (atomic.LoadUint32(&closing) > 0)
 }
