fc7bda2b92984b437164188ed27cfd702ab6b997
Kyle Thomas <kyle@provide.services>
2020-04-29 02:49:26 -0400

    Add splunk/unibright connectors

---
+++ connector/provider/unibright.go
@@ -0,0 +1,224 @@
+package provider
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	ipfs "github.com/ipfs/go-ipfs-api"
+	"github.com/jinzhu/gorm"
+
+	natsutil "github.com/kthomas/go-natsutil"
+	uuid "github.com/kthomas/go.uuid"
+	"github.com/provideapp/goldmine/common"
+	"github.com/provideapp/goldmine/network"
+)
+
+// UnibrightProvider is a connector.ProviderAPI implementing orchestration for Provide
+type UnibrightProvider struct {
+	connectorID    uuid.UUID
+	model          *gorm.DB
+	config         map[string]interface{}
+	networkID      *uuid.UUID
+	applicationID  *uuid.UUID
+	organizationID *uuid.UUID
+	region         *string
+	apiURL         *string
+	apiPort        int
+}
+
+// InitUnibrightProvider initializes and returns the Provide connector API provider
+func InitUnibrightProvider(connectorID uuid.UUID, networkID, applicationID, organizationID *uuid.UUID, model *gorm.DB, config map[string]interface{}) *UnibrightProvider {
+	region, regionOk := config["region"].(string)
+	apiURL, _ := config["api_url"].(string)
+	apiPort, apiPortOk := config["api_port"].(float64)
+	if connectorID == uuid.Nil || !regionOk || !apiPortOk || networkID == nil || *networkID == uuid.Nil {
+		return nil
+	}
+	return &UnibrightProvider{
+		connectorID:    connectorID,
+		model:          model,
+		config:         config,
+		networkID:      networkID,
+		applicationID:  applicationID,
+		organizationID: organizationID,
+		region:         common.StringOrNil(region),
+		apiURL:         common.StringOrNil(apiURL),
+		apiPort:        int(apiPort),
+	}
+}
+
+func (p *UnibrightProvider) apiClientFactory(basePath *string) *ipfs.Shell {
+	uri := ""
+	if basePath != nil {
+		uri = *basePath
+	}
+	apiURL := p.apiURLFactory(uri)
+	if apiURL == nil {
+		common.Log.Warningf("unable to initialize Provide api client factory")
+		return nil
+	}
+
+	return ipfs.NewShell(*apiURL)
+}
+
+func (p *UnibrightProvider) apiURLFactory(path string) *string {
+	if p.apiURL == nil {
+		return nil
+	}
+
+	suffix := ""
+	if path != "" {
+		suffix = fmt.Sprintf("/%s", path)
+	}
+	return common.StringOrNil(fmt.Sprintf("%s%s", *p.apiURL, suffix))
+}
+
+func (p *UnibrightProvider) rawConfig() *json.RawMessage {
+	cfgJSON, _ := json.Marshal(p.config)
+	_cfgJSON := json.RawMessage(cfgJSON)
+	return &_cfgJSON
+}
+
+// Deprovision undeploys all associated nodes and load balancers and removes them from the Provide connector
+func (p *UnibrightProvider) Deprovision() error {
+	loadBalancers := make([]*network.LoadBalancer, 0)
+	p.model.Association("LoadBalancers").Find(&loadBalancers)
+	for _, balancer := range loadBalancers {
+		p.model.Association("LoadBalancers").Delete(balancer)
+	}
+
+	nodes := make([]*network.Node, 0)
+	p.model.Association("Nodes").Find(&nodes)
+	for _, node := range nodes {
+		common.Log.Debugf("Attempting to deprovision node %s on connector: %s", node.ID, p.connectorID)
+		p.model.Association("Nodes").Delete(node)
+		node.Delete()
+	}
+
+	for _, balancer := range loadBalancers {
+		msg, _ := json.Marshal(map[string]interface{}{
+			"load_balancer_id": balancer.ID,
+		})
+		natsutil.NatsStreamingPublish(natsLoadBalancerDeprovisioningSubject, msg)
+	}
+
+	return nil
+}
+
+// Provision configures a new load balancer and the initial Provide nodes and associates the resources with the Provide connector
+func (p *UnibrightProvider) Provision() error {
+	loadBalancer := &network.LoadBalancer{
+		NetworkID:      *p.networkID,
+		ApplicationID:  p.applicationID,
+		OrganizationID: p.organizationID,
+		Type:           common.StringOrNil(ProvideConnectorProvider),
+		Description:    common.StringOrNil(fmt.Sprintf("Provide Connector Load Balancer")),
+		Region:         p.region,
+		Config:         p.rawConfig(),
+	}
+
+	if loadBalancer.Create() {
+		common.Log.Debugf("Created load balancer %s on connector: %s", loadBalancer.ID, p.connectorID)
+		p.model.Association("LoadBalancers").Append(loadBalancer)
+
+		msg, _ := json.Marshal(map[string]interface{}{
+			"connector_id": p.connectorID,
+		})
+		natsutil.NatsStreamingPublish(natsConnectorDenormalizeConfigSubject, msg)
+
+		err := p.ProvisionNode()
+		if err != nil {
+			common.Log.Warning(err.Error())
+		}
+	} else {
+		return fmt.Errorf("Failed to provision load balancer on connector: %s; %s", p.connectorID, *loadBalancer.Errors[0].Message)
+	}
+
+	return nil
+}
+
+// DeprovisionNode undeploys an existing node removes it from the Provide connector
+func (p *UnibrightProvider) DeprovisionNode() error {
+	node := &network.Node{}
+	p.model.Association("Nodes").Find(node)
+
+	return nil
+}
+
+// ProvisionNode deploys and load balances a new node and associates it with the Provide connector
+func (p *UnibrightProvider) ProvisionNode() error {
+	node := &network.Node{
+		NetworkID:      *p.networkID,
+		ApplicationID:  p.applicationID,
+		OrganizationID: p.organizationID,
+		Config:         p.rawConfig(),
+	}
+
+	if node.Create() {
+		common.Log.Debugf("Created node %s on connector: %s", node.ID, p.connectorID)
+		p.model.Association("Nodes").Append(node)
+
+		loadBalancers := make([]*network.LoadBalancer, 0)
+		p.model.Association("LoadBalancers").Find(&loadBalancers)
+		for _, balancer := range loadBalancers {
+			msg, _ := json.Marshal(map[string]interface{}{
+				"load_balancer_id": balancer.ID.String(),
+				"node_id":          node.ID.String(),
+			})
+			natsutil.NatsStreamingPublish(natsLoadBalancerBalanceNodeSubject, msg)
+		}
+
+		msg, _ := json.Marshal(map[string]interface{}{
+			"connector_id": p.connectorID.String(),
+		})
+		natsutil.NatsStreamingPublish(natsConnectorResolveReachabilitySubject, msg)
+	} else {
+		return fmt.Errorf("Failed to provision node on connector: %s", p.connectorID)
+	}
+
+	return nil
+}
+
+// Reachable returns true if the Provide API provider is available
+func (p *UnibrightProvider) Reachable() bool {
+	loadBalancers := make([]*network.LoadBalancer, 0)
+	p.model.Association("LoadBalancers").Find(&loadBalancers)
+	for _, loadBalancer := range loadBalancers {
+		if loadBalancer.ReachableOnPort(uint(p.apiPort)) {
+			return true
+		}
+	}
+	common.Log.Debugf("Connector is unreachable: %s", p.connectorID)
+	return false
+}
+
+// Create impl for UnibrightProvider
+func (p *UnibrightProvider) Create(params map[string]interface{}) (*ConnectedEntity, error) {
+	return nil, errors.New("create not implemented for Provide connectors")
+}
+
+// Find impl for UnibrightProvider
+func (p *UnibrightProvider) Find(id string) (*ConnectedEntity, error) {
+	return nil, errors.New("read not implemented for Provide connectors")
+}
+
+// Update impl for UnibrightProvider
+func (p *UnibrightProvider) Update(id string, params map[string]interface{}) error {
+	return errors.New("update not implemented for Provide connectors")
+}
+
+// Delete impl for UnibrightProvider
+func (p *UnibrightProvider) Delete(id string) error {
+	return errors.New("delete not implemented for Provide connectors")
+}
+
+// List impl for UnibrightProvider
+func (p *UnibrightProvider) List(params map[string]interface{}) ([]*ConnectedEntity, error) {
+	return nil, errors.New("list not implemented for Provide connectors")
+}
+
+// Query impl for UnibrightProvider
+func (p *UnibrightProvider) Query(q string) (interface{}, error) {
+	return nil, errors.New("query not implemented for Provide connectors")
+}
