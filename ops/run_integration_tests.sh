b8f2d0df1930a8b42d44d6cacf67c26dc14aa686
Kyle Thomas <kyle@provide.services>
2020-11-23 12:55:26 -0500

    Uncomment lines within run_integration_tests.sh

--- ops/run_integration_tests.sh
+++ ops/run_integration_tests.sh
@@ -4,5 +4,5 @@ docker-compose -f ./ops/docker-compose.yml build --no-cache nchain
 docker-compose -f ./ops/docker-compose.yml up -d
 TAGS=integration ./ops/run_local_tests.sh
 # docker-compose -f ./ops/docker-compose.yml logs
-# docker-compose -f ./ops/docker-compose.yml down
-# docker volume rm ops_provide-db
+docker-compose -f ./ops/docker-compose.yml down
+docker volume rm ops_provide-db
