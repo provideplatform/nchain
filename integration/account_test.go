db2cdca862f97a0cf7910cbf237ad0026d3973ab
Kyle Thomas <kyle@provide.services>
2020-11-27 13:17:48 -0500

    Fix account integration spec

--- integration/account_test.go
+++ integration/account_test.go
@@ -3,64 +3,12 @@
 package integration
 
 import (
-	"encoding/json"
-	"fmt"
-	"math/big"
 	"testing"
-	"time"
 
 	uuid "github.com/kthomas/go.uuid"
-	"github.com/provideservices/provide-go/api"
 	ident "github.com/provideservices/provide-go/api/ident"
 	nchain "github.com/provideservices/provide-go/api/nchain"
 )
-
-// Account contains the specific account user details
-type Account struct {
-	api.Model
-	NetworkID      *uuid.UUID `json:"network_id,omitempty"`
-	WalletID       *uuid.UUID `json:"wallet_id,omitempty"`
-	ApplicationID  *uuid.UUID `json:"application_id,omitempty"`
-	UserID         *uuid.UUID `json:"user_id,omitempty"`
-	OrganizationID *uuid.UUID `json:"organization_id,omitempty"`
-
-	VaultID *uuid.UUID `json:"vault_id,omitempty"`
-	KeyID   *uuid.UUID `json:"key_id,omitempty"`
-
-	Type *string `json:"type,omitempty"`
-
-	HDDerivationPath *string `json:"hd_derivation_path,omitempty"` // i.e. m/44'/60'/0'/0
-	PublicKey        *string `json:"public_key,omitempty"`
-	PrivateKey       *string `json:"private_key,omitempty"`
-
-	Address    string         `json:"address"`
-	Balance    *big.Int       `json:"balance,omitempty"`
-	AccessedAt *time.Time     `json:"accessed_at,omitempty"`
-	Wallet     *nchain.Wallet `json:"-"`
-}
-
-// GoCreateAccount creates a new account
-func GoCreateAccount(token string, params map[string]interface{}) (*Account, error) {
-	uri := "accounts"
-	status, resp, err := nchain.InitNChainService(token).Post(uri, params)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if status != 201 {
-		return nil, fmt.Errorf("failed to create account. status: %v", status)
-	}
-
-	account := &Account{}
-	accountRaw, _ := json.Marshal(resp)
-	err = json.Unmarshal(accountRaw, &account)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create account. status: %v; %s", status, err.Error())
-	}
-
-	return account, nil
-}
 
 func TestListAccounts(t *testing.T) {
 	testId, err := uuid.NewV4()
@@ -135,7 +83,7 @@ func TestListAccounts(t *testing.T) {
 		}
 
 		// create the account for that user, for the Ropsten network
-		account, err := GoCreateAccount(*appToken.Token, map[string]interface{}{
+		account, err := nchain.CreateAccount(*appToken.Token, map[string]interface{}{
 			"network_id":     ropstenNetworkID,
 			"application_id": app.ID,
 		})
