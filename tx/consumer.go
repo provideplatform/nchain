841593b9e1641cab64d38eac9838f722cffff4f2
eoinco <eoin.connolly@gmail.com>
2021-03-24 15:32:17 +0000

    ensure block number present when tx receipt processed

--- tx/consumer.go
+++ tx/consumer.go
@@ -840,6 +840,21 @@ func consumeTxReceiptMsg(msg *stan.Msg) {
 		natsutil.AttemptNack(msg, txReceiptMsgTimeout)
 	} else {
 		common.Log.Debugf("fetched tx receipt for hash: %s", *tx.Hash)
+
+		// w00t but potentially HACKYHACK...
+		// TODO go through the flow of the recept and see why some get a block
+		// and some don't
+
+		blockNumber := tx.Response.Receipt.(*provide.TxReceipt).BlockNumber
+		// if we have a block number in the receipt, and the tx has no block
+		// populate the block and finalized timestamp
+		if blockNumber != nil && tx.Block == nil {
+			receiptBlock := blockNumber.Uint64()
+			tx.Block = &receiptBlock
+			receiptFinalized := time.Now()
+			tx.FinalizedAt = &receiptFinalized
+			common.Log.Debugf("*** tx hash %s finalized in block %v at %s", *tx.Hash, blockNumber, receiptFinalized.Format("Mon, 02 Jan 2006 15:04:05 MST"))
+		}
 		tx.updateStatus(db, "success", nil)
 		msg.Ack()
 	}
