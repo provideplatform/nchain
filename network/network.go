dc3630f7a184b8412b0ac067384c763ead0b950f
Kyle Thomas <kyle@provide.services>
2021-05-03 02:09:59 -0400

    Add isPublic method to network; allow public contracts to be resolved by address

--- network/network.go
+++ network/network.go
@@ -203,6 +203,27 @@ func (n *Network) encryptConfig() bool {
 	return true
 }
 
+func (n *Network) IsPublic() bool {
+	if n.IsEthereumNetwork() {
+		switch n.ID.String() {
+		case "deca2436-21ba-4ff5-b225-ad1b0b2f5c59":
+			return true
+		case "07102258-5e49-480e-86af-6d0c3260827d":
+			return true
+		case "66d44f30-9092-4182-a3c4-bc02736d6ae5":
+			return true
+		case "8d31bf48-df6b-4a71-9d7c-3cb291111e27":
+			return true
+		case "1b16996e-3595-4985-816c-043345d22f8c":
+			return true
+		default:
+			return false
+		}
+	}
+
+	return false
+}
+
 func (n *Network) PaymentsNetworkName() *string {
 	if n.IsEthereumNetwork() {
 		switch n.ID.String() {
