a354f5957be61e77caee426838d61c96f474dbd3
eoinco <eoin.connolly@gmail.com>
2021-05-26 19:46:17 +0100

    updating for account path (kovan test)

--- integration/integration_basic_kovan_test.go
+++ integration/integration_basic_kovan_test.go
@@ -14,7 +14,7 @@ import (
 	nchain "github.com/provideservices/provide-go/api/nchain"
 )
 
-func TestContractHDWalletKovanApp(t *testing.T) {
+func _TestContractHDWalletKovanApp(t *testing.T) {
 
 	t.Parallel()
 
@@ -239,7 +239,6 @@ func TestContractHDWalletKovanApp(t *testing.T) {
 	}
 }
 
-// XXX temporarily disabled to reduce test noise
 func _TestContractHDWalletKovanOrg(t *testing.T) {
 
 	t.Parallel()
@@ -438,6 +437,238 @@ func _TestContractHDWalletKovanOrg(t *testing.T) {
 
 			if err != nil {
 				t.Errorf("error executing contract: %s", err.Error())
+				return
+			}
+		}
+
+		t.Logf("contract execution successful")
+	}
+}
+
+func TestContractAccountKovanApp(t *testing.T) {
+
+	t.Parallel()
+
+	testId, err := uuid.NewV4()
+	if err != nil {
+		t.Logf("error creating new UUID")
+	}
+
+	userToken, err := UserAndTokenFactory(testId)
+	if err != nil {
+		t.Errorf("user authentication failed. Error: %s", err.Error())
+	}
+
+	testcaseApp := Application{
+		"app" + testId.String(),
+		"appdesc " + testId.String(),
+	}
+
+	app, err := appFactory(*userToken, testcaseApp.name, testcaseApp.description)
+	if err != nil {
+		t.Errorf("error setting up application. Error: %s", err.Error())
+		return
+	}
+
+	appToken, err := appTokenFactory(*userToken, app.ID)
+	if err != nil {
+		t.Errorf("error getting app token. Error: %s", err.Error())
+		return
+	}
+
+	// wallet, err := nchain.CreateWallet(*appToken.Token, map[string]interface{}{
+	// 	"mnemonic": "traffic charge swing glimpse will citizen push mutual embrace volcano siege identify gossip battle casual exit enrich unlock muscle vast female initial please day",
+	// })
+	// if err != nil {
+	// 	t.Errorf("error creating wallet: %s", err.Error())
+	// 	return
+	// }
+
+	// this path produces the ETH address 0x6af845bae76f5cc16bc93f86b83e8928c3dfda19
+	//path := `m/44'/60'/2'/0/0`
+
+	// load the ekho compiled artifact
+	ekhoArtifact, err := ioutil.ReadFile("artifacts/ekho.json")
+	if err != nil {
+		t.Errorf("error loading ekho artifact. Error: %s", err.Error())
+	}
+
+	ekhoCompiledArtifact := nchain.CompiledArtifact{}
+	err = json.Unmarshal(ekhoArtifact, &ekhoCompiledArtifact)
+	if err != nil {
+		t.Errorf("error converting ekho compiled artifact. Error: %s", err.Error())
+	}
+
+	// load the readwrite compiled artifact
+	rwArtifact, err := ioutil.ReadFile("artifacts/readwritetester.json")
+	if err != nil {
+		t.Errorf("error loading readwritetester artifact. Error: %s", err.Error())
+	}
+
+	rwCompiledArtifact := nchain.CompiledArtifact{}
+	err = json.Unmarshal(rwArtifact, &rwCompiledArtifact)
+	if err != nil {
+		t.Errorf("error converting readwritetester compiled artifact. Error: %s", err.Error())
+	}
+
+	tt := []struct {
+		network  string
+		name     string
+		artifact nchain.CompiledArtifact
+	}{
+		{kovanNetworkID, "ekho", ekhoCompiledArtifact},
+		{kovanNetworkID, "readwrite", rwCompiledArtifact},
+	}
+
+	for _, tc := range tt {
+		// create unique references
+		txRef, err := uuid.NewV4()
+		if err != nil {
+			t.Errorf("error creating unique tx ref. Error: %s", err.Error())
+			return
+		}
+
+		contractRef, err := uuid.NewV4()
+		if err != nil {
+			t.Errorf("error creating unique contract ref. Error: %s", err.Error())
+			return
+		}
+
+		// create the account for that user, for the Ropsten network
+		account, err := nchain.CreateAccount(*appToken.Token, map[string]interface{}{
+			"network_id":     tc.network,
+			"application_id": app.ID,
+		})
+
+		if err != nil {
+			t.Errorf("error creating user account. Error: %s", err.Error())
+		}
+
+		t.Logf("creating contract using account id: %s, ref: %s", account.ID, contractRef)
+		contract, err := nchain.CreateContract(*appToken.Token, map[string]interface{}{
+			"network_id":     tc.network,
+			"application_id": app.ID.String(),
+			"account_id":     account.ID.String(),
+			"name":           tc.name,
+			"address":        "0x",
+			"params": map[string]interface{}{
+				"account_id":        account.ID.String(),
+				"compiled_artifact": tc.artifact,
+				"ref":               contractRef.String(),
+			},
+		})
+		if err != nil {
+			t.Errorf("error creating %s contract. Error: %s", tc.name, err.Error())
+			return
+		}
+
+		// wait for the contract to be deployed
+		started := time.Now().Unix()
+		for {
+			if time.Now().Unix()-started >= contractTimeout {
+				t.Errorf("timed out awaiting contract address for %s contract for %s network", tc.name, tc.network)
+				return
+			}
+
+			cntrct, err := nchain.GetContractDetails(*appToken.Token, contract.ID.String(), map[string]interface{}{})
+			if err != nil {
+				t.Errorf("error fetching %s contract details; %s", tc.name, err.Error())
+				return
+			}
+
+			if cntrct.Address != nil && *cntrct.Address != "0x" {
+				t.Logf("%s contract address resolved; contract id: %s; address: %s", tc.name, cntrct.ID.String(), *cntrct.Address)
+				break
+			}
+
+			t.Logf("resolving contract for %s network ...", tc.network)
+			time.Sleep(contractSleepTime * time.Second)
+		}
+
+		// create a message for contract
+		msg := common.RandomString(118)
+		t.Logf("msg: %s", msg)
+		t.Logf("executing contract using account id: %s", account.ID.String())
+
+		params := map[string]interface{}{}
+
+		switch tc.name {
+		case "ekho":
+			parameter := fmt.Sprintf(`{"method":"broadcast", "params": ["%s"], "value":0, "account_id":"%s", "ref": "%s"}`, msg, account.ID, txRef)
+			t.Logf("parameter is: %s", parameter)
+			json.Unmarshal([]byte(parameter), &params)
+		case "readwrite":
+			parameter := fmt.Sprintf(`{"method":"setString", "params": ["%s"], "value":0, "account_id":"%s", "ref": "%s"}`, msg, account.ID, txRef)
+			t.Logf("parameter is: %s", parameter)
+			json.Unmarshal([]byte(parameter), &params)
+		}
+
+		// execute the contract method
+		t.Logf("%s Executing contract using params %+v", time.Now(), params)
+		t.Logf("executing transaction using tx ref: %s", txRef)
+		execResponse, err := nchain.ExecuteContract(*appToken.Token, contract.ID.String(), params)
+		if err != nil {
+			t.Logf("%s got error %s, and execresponse is: %v", time.Now(), err.Error(), execResponse)
+			t.Logf("error executing contract1, but hey, maybe it's not an error Error: %s", err.Error())
+		}
+
+		// wait for the transaction to be mined (get a tx hash)
+		started = time.Now().Unix()
+		for {
+			if time.Now().Unix()-started >= transactionTimeout {
+				t.Error("timed out awaiting transaction hash")
+				return
+			}
+
+			tx, err := nchain.GetTransactionDetails(*appToken.Token, txRef.String(), map[string]interface{}{})
+			//this is populated by nchain consumer, so it can take a moment to appear, so we won't quit right away on a 404
+			if err != nil {
+				t.Logf("tx not yet mined...")
+			}
+
+			if err == nil {
+				if tx.Description != nil {
+					t.Errorf("tx error; tx id: %s; Error: %s", tx.ID.String(), *tx.Description)
+					return
+				}
+				if tx.Block != nil && *tx.Hash != "0x" {
+					t.Logf("tx resolved; tx id: %s; hash: %s; block: %d", tx.ID.String(), *tx.Hash, *tx.Block)
+					break
+				}
+				t.Logf("resolving transaction...")
+			}
+
+			time.Sleep(transactionSleepTime * time.Second)
+		}
+
+		// now we also run a readonly transaction for some of the contracts
+		if tc.name == "readwrite" {
+			// HACK, we should only check this when there is a block associated with the tx id!
+			time.Sleep(10 * time.Second)
+			params := map[string]interface{}{}
+			parameter := fmt.Sprintf(`{"method":"getString", "params": [""], "value":0, "account_id":"%s"}`, account.ID.String())
+			json.Unmarshal([]byte(parameter), &params)
+
+			// execute the contract method
+			execResponse, err := nchain.ExecuteContract(*appToken.Token, contract.ID.String(), params)
+			if err != nil {
+				t.Errorf("error executing contract3. Error: %s", err.Error())
+				return
+			}
+			if execResponse.Response != nil {
+				t.Logf("execution response: %v", execResponse.Response)
+				if execResponse.Response != msg {
+					t.Errorf("expected msg %s returned. got %v", msg, execResponse.Response)
+					return
+				}
+			}
+			if execResponse.Response == nil {
+				t.Errorf("expected msg returned, got nil response")
+				return
+			}
+
+			if err != nil {
+				t.Errorf("error executing contract4: %s", err.Error())
 				return
 			}
 		}
--- tx/tx.go
+++ tx/tx.go
@@ -263,7 +263,13 @@ func generateSignedTx(wg *sync.WaitGroup, txs *TransactionSigner, tx *Transactio
 	})
 
 	if err != nil {
-		err = fmt.Errorf("failed to sign %d-byte transaction payload using hardened account for HD wallet: %s; %s", len(hash), txs.Wallet.ID, err.Error())
+		// change the message depending on whether we're using a wallet or an account (TODO: correc this)
+		if txs.Wallet != nil {
+			err = fmt.Errorf("failed to sign %d-byte transaction payload using hardened account for HD wallet: %s; %s", len(hash), txs.Wallet.ID, err.Error())
+		} else {
+			err = fmt.Errorf("failed to sign %d-byte transaction payload using account ID: %s; %s", len(hash), txs.Account.ID, err.Error())
+		}
+
 		common.Log.Debugf("%s", err.Error())
 		common.Log.Warning(err.Error())
 		return nil, nil, nil, err
