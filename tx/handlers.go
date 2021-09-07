98a482f9e143451e5615032173c155b7e3d4a253
Kyle Thomas <kyle@provide.services>
2021-09-06 22:51:29 -0400

    Update tx user validation

--- tx/handlers.go
+++ tx/handlers.go
@@ -150,7 +150,7 @@ func transactionDetailsHandler(c *gin.Context) {
 
 	validApp := appID != nil && (tx.ApplicationID != nil && *tx.ApplicationID == *appID)
 	validOrg := orgID != nil && (tx.OrganizationID != nil && *tx.OrganizationID == *orgID)
-	validUser := userID != nil && (tx.UserID != nil && *tx.UserID != *userID)
+	validUser := userID != nil && (tx.UserID != nil && *tx.UserID == *userID)
 
 	if !validApp && !validOrg && !validUser {
 		provide.RenderError("forbidden", 403, c)
