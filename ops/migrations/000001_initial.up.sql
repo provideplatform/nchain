fbabadc494a1ee3af67a5c0044acb3d87dac2d72
Philip Keiter <FiretronP75@users.noreply.github.com>
2022-06-01 16:37:59 -0500

    Update license (#29)
    Update license

--- ops/migrations/000001_initial.up.sql
+++ ops/migrations/000001_initial.up.sql
@@ -1058,6 +1058,23 @@ ALTER TABLE ONLY public.tokens
     ADD CONSTRAINT tokens_sale_contract_id_contracts_id_foreign FOREIGN KEY (sale_contract_id) REFERENCES public.contracts(id) ON UPDATE CASCADE ON DELETE SET NULL;
 
 
+/*
+ * Copyright 2017-2022 Provide Technologies Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 --
 -- Name: transactions transactions_account_id_accounts_id_foreign; Type: FK CONSTRAINT; Schema: public; Owner: nchain
 --
